250b813193b4c55c5b9c380a9ef686bb7959bd8d
Matthew Mihok <4D.69.68.6F.6B@gmail.com>
2016-11-11 22:17:58 -0500

    adding superficial test

---
+++ commands/get_test.go
@@ -0,0 +1,12 @@
+package person
+
+import "testing"
+
+func TestGet(t *testing.T) {
+    // <setup code>
+    t.Run("Get does something", func(t *testing.T) {
+      // <test code>
+      // t.Fatalf("BOOM!")
+    })
+    // <tear-down code>
+}
